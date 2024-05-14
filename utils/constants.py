13434a8207bfb11c5df8bfc243b071a20367ae0e
Carlos Hernandez <carlos.hernandez@validatedid.com>
2024-05-14 14:40:46 -0400

    refactor: moved function and constant to modules

---
+++ utils/constants.py
@@ -0,0 +1,72 @@
+FAKE_DATA = {
+    "components": [
+        {
+            "name": "fundamental_frequency",
+            "results": [
+                {
+                    "emotion": "anger",
+                    "percentage": "80"
+                },
+                {
+                    "emotion": "sadness",
+                    "percentage": "85"
+                },
+                {
+                    "emotion": "fear",
+                    "percentage": "92"
+                }
+            ]
+        },
+        {
+            "name": "first_formant",
+            "results": [
+                {
+                    "emotion": "anger",
+                    "percentage": "80"
+                },
+                {
+                    "emotion": "sadness",
+                    "percentage": "85"
+                },
+                {
+                    "emotion": "fear",
+                    "percentage": "92"
+                }
+            ]
+        },
+        {
+            "name": "second_formant",
+            "results": [
+                {
+                    "emotion": "anger",
+                    "percentage": "80"
+                },
+                {
+                    "emotion": "sadness",
+                    "percentage": "85"
+                },
+                {
+                    "emotion": "fear",
+                    "percentage": "92"
+                }
+            ]
+        },
+        {
+            "name": "third_formant",
+            "results": [
+                {
+                    "emotion": "anger",
+                    "percentage": "80"
+                },
+                {
+                    "emotion": "sadness",
+                    "percentage": "85"
+                },
+                {
+                    "emotion": "fear",
+                    "percentage": "92"
+                }
+            ]
+        },
+    ]
+}
