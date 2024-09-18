565cc916bf3260df613fea59ad28180b1a982ad9
Carlos Hernandez <carlos.hernandez@validatedid.com>
2024-09-18 18:51:15 -0400

    docs: removed innecessary files and added comments

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
+# Se importan librerías
 from flask import Flask, request, jsonify
 import base64
-import pywt
-import pickle
-
-from utils.constants import FAKE_DATA
-from utils.load_audio import load_audio
-from utils.plot_spectrum import plot_magnitude_spectrum
-from utils.get_wavelet_transform import get_wavelet_transform
-from sklearn.preprocessing import OneHotEncoder
-from utils.mfcc import extract_mfcc
 from tensorflow.keras.models import load_model
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
+from utils.mfcc import extract_mfcc
+from utils.load_audio import load_audio
+
+# Inicialización de la aplicación
 app = Flask(__name__)
 
-@app.route('/detectar',  methods=['POST', 'GET'])
+# Escuchar endpoint POST /detectar
+@app.route('/detectar',  methods=['POST'])
 def detect():
     if request.method == 'POST':
+        # Se lee el audio en formato base64 proveniente del body de la petición
         body = request.get_json()
         decoded_audio_data = body['audio_data']
 
-        ## Convertir audio de base 64 a vector unidimensional
+        # Convertir audio de base 64 a vector unidimensional
         decode_string = base64.b64decode(decoded_audio_data)
-        wav_file = open("temp.wav", "wb")
-        wav_file.write(decode_string)
+        with open("temp.wav", "wb") as wav_file:
+            wav_file.write(decode_string)
 
         file_name = "./temp.wav"
+        # Se lee el audio .wav y se imprime información relevante sobre el mismo
         header_chunk_id , header_chunk_size, header_chunk_format, format_chunk_id, format_chunk_size, audio_data, format_code, channels, sample_width, sample_rate, byte_rate, block_align, data_chunk_id, data_chunk_size = load_audio(file_name)
 
         number_of_samples = len(audio_data)
@@ -51,35 +51,38 @@ def detect():
         print(f"Data : {audio_data}")
         print(f"Type of data array : {type(audio_data[0])}")  
 
+        # Se obtienen los parámetros MFCC del audio que se está procesando
         mfcc_data = { 'speech': file_name }
         df = pd.DataFrame(data=mfcc_data, index=[0])
         x_mfcc = df['speech'].apply(lambda x: extract_mfcc(x))
-        X= [x for x in x_mfcc]
+
+        # Se preparan los datos para introducirlos en el modelo de predicción
+        X = [x for x in x_mfcc]
         X = np.array(X)
         X = np.expand_dims(X, -1)
         print(X, X.shape)
 
+        # Se importa el modelo
         model = load_model('model.h5')
 
-        # evaluate model 
+        # Se realiza la predicción
         pred_test = model.predict(X)
 
-        print(pred_test[0], 'HERE')
-
+        # Se crea el encoder para decodificar la predicción
         df = pd.read_csv('data.csv')
-
         enc = OneHotEncoder()
         enc.fit_transform(df[['label']])
 
+        # Se decodifica la predicción
         y_pred = enc.inverse_transform(pred_test)
 
+        # Se envía la predicción como respuesta a la petición
         prediction = y_pred[0]
-
         print('The predicition is ', prediction[0])
+        response = jsonify({ "prediction": prediction[0] })
+        response.headers.add('Access-Control-Allow-Origin', '*')
 
-        return jsonify({ "prediction": prediction[0] })
-    else:
-        return jsonify(FAKE_DATA)
+        return response
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(port=8080, debug=True, host='0.0.0.0')
